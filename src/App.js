b059b39e523407467e1dd72ba6fe1039147ba6df
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-26 16:21:56 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sample from "~/components/Sample";
+
 function App() {
   return (
     <div className="App">
